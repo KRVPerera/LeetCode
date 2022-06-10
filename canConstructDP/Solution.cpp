ada8c76f22b2b60c42eb6013a847e265dbe78d65
KRVPerera <rukshan.viduranga@gmail.com>
2022-06-11 01:38:10 +0530

    Solved canConstruct with DP

--- canConstructDP/Solution.cpp
+++ canConstructDP/Solution.cpp
@@ -7,15 +7,42 @@
 
 using namespace std;
 
-bool canConstruct(string st, vector<string> stringArray) {
+bool canConstructHelper(string st, vector<string>& stringArray, unordered_map<string, bool>& mem) {
+    if (st.empty()) {
+        return true;
+    }
+    auto found = mem.find(st);
+    if (found != mem.end()) {
+        return found->second;
+    }
+
     auto arSize = stringArray.size();
     for (int i = 0; i < arSize; ++i) {
-        auto f = st.find(stringArray[i]);
-        cout << stringArray[i] << " : " << f << endl;
+        string wordPart = stringArray[i];
+        auto f = st.find(wordPart);
+        if (f > st.size()) {
+            continue;
+        }
+        if (f == 0) {
+            string subStrRight = st.substr(wordPart.size());
+            if (canConstructHelper(subStrRight, stringArray, mem)) {
+                mem[st] = true;
+                return true;
+            }
+        }
     }
-    return true;
+
+    mem[st] = false;
+    return false;
+}
+
+bool canConstruct(string st, vector<string> stringArray) {
+    unordered_map<string, bool> mem;
+    return canConstructHelper(st, stringArray, mem);
 }
 
 TEST_CASE( "canConstruct abcdef", "[b]" ) {
-    REQUIRE( canConstruct("abcdef", {"ab", "abc", "cd", "def", "abcd"}) == true );
+    REQUIRE( canConstruct("abcdef", {"ab", "abc", "cd", "def", "abcd", "kf"}) == true );
+    REQUIRE( canConstruct("skateboard", {"bo", "rd", "ate", "def", "abcd", "kf"}) == false );
+    REQUIRE( canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", {"ee", "eee", "e", "eeee", "eeeee", "eeeeee"}) == false );
 }
